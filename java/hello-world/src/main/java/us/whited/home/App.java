134c6c20a806f89d9f3d290dc70783e45c587cdf
Matthew Whited <matt@whited.us>
2022-01-01 21:11:53 -0500

    update java example

--- java/hello-world/src/main/java/us/whited/home/App.java
+++ java/hello-world/src/main/java/us/whited/home/App.java
@@ -1,13 +1,32 @@
 package us.whited.home;
 
+import com.github.fracpete.simpleargparse4j.ArgumentParser;
+import com.github.fracpete.simpleargparse4j.ArgumentParserException;
+import com.github.fracpete.simpleargparse4j.Namespace;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        // https://github.com/fracpete/simple-argparse4j
+        ArgumentParser parser = new ArgumentParser("create");
+        parser.addOption("--test").dest("test").help("name of the environment")
+                .required(true);
+
+        try
+        {
+            Namespace ns = parser.parseArgs(args);
+
+            System.out.println("test: " + ns.getString("test"));
+            System.out.println("Hello World!");
+        }
+        catch (ArgumentParserException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
